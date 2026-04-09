d4c7fef20c458b2b2217cdbc097878aa89cdc97f
ivansigala <diego.sigala@iteso.mx>
2026-04-09 05:22:08 -0600

    Updated read and write register functions
    Signed-off-by: ivansigala <diego.sigala@iteso.mx>

--- mpu_9250.c
+++ mpu_9250.c
@@ -18,9 +18,32 @@ void mpu9250_init(void* callback){
 }
 
 void mpu9250_read_reg(spi_ctrl_t *imu, uint8_t reg, uint8_t* data, size_t length){
-    spi_master_transfer(imu, &reg, data, length);
+    if (length == 0 || length >= MPU9250_MAX_TX_RX_LEN) return;
+
+    uint8_t tx_buf[MPU9250_MAX_TX_RX_LEN] = {0};
+    uint8_t rx_buf[MPU9250_MAX_TX_RX_LEN] = {0};
+
+    // Set MSB to 1 for SPI Read
+    tx_buf[0] = reg | MPU9250_SPI_READ_BIT; 
+    
+    // Total transfer length is the 1 address byte + 'length' data bytes
+    spi_master_transfer(imu, tx_buf, rx_buf, length + 1);
+
+    // The first byte received is during the address transmission, so skip rx_buf[0]
+    memcpy(data, &rx_buf[1], length);
 }
 
 void mpu9250_write_reg(spi_ctrl_t *imu, uint8_t reg, uint8_t* data, size_t length){
-    spi_master_transfer(imu, &reg, data, length);
+    if (length == 0 || length >= MPU9250_MAX_TX_RX_LEN) return;
+
+    uint8_t tx_buf[MPU9250_MAX_TX_RX_LEN] = {0};
+
+    // Set MSB to 0 for SPI Write
+    tx_buf[0] = reg & ~MPU9250_SPI_READ_BIT; 
+    
+    // Append the payload data to the transmit buffer
+    memcpy(&tx_buf[1], data, length);
+
+    // For a write, we don't care about the rx_buf
+    spi_master_transfer(imu, tx_buf, NULL, length + 1);
 }
