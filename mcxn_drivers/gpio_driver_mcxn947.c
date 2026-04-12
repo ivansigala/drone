25da0a4e71d83a240a03d1f86df8bb8aa799aeb9
ivansigala <diego.sigala@iteso.mx>
2026-04-12 17:23:01 -0600

    Changed pin config in gpio init
    Signed-off-by: ivansigala <diego.sigala@iteso.mx>

--- mcxn_drivers/gpio_driver_mcxn947.c
+++ mcxn_drivers/gpio_driver_mcxn947.c
@@ -179,7 +179,7 @@ void gpio_setup(GPIO_Type* gpio_base, PORT_Type *port_base, uint8_t pin)
 
     const port_pin_config_t port_config = {
         kPORT_PullDown,             // Pull-down for your 3.3V wire test
-        kPORT_LowPullResistor,
+        kPORT_HighPullResistor,
         kPORT_FastSlewRate,
         kPORT_PassiveFilterDisable,
         kPORT_OpenDrainDisable,
